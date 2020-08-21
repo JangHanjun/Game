01e2c83bcfcc53c251e2ea3120f42543cdd4bc78
JangHanjun <68003176+JangHanjun@users.noreply.github.com>
2020-08-21 15:56:41 +0900

    Add files via upload

---
+++ ChatBox/NpcSentence.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcSentence : MonoBehaviour
+{
+    public string[] sentences;
+    public Transform chatTr;
+    public GameObject chatBoxPrefab;
+
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if(collision.gameObject.tag == "Player") {
+            TalkNpc();
+        } 
+    }
+
+    private void Start() {
+        
+    }
+
+    public void TalkNpc() {
+        GameObject go = Instantiate(chatBoxPrefab);
+        go.GetComponent<ChatSystem>().Ondialogue(sentences, chatTr);
+    }
+
+
+}
