b96d6ef6d4aba1f8d341aea15dbd610a8c611444
JangHanjun <68003176+JangHanjun@users.noreply.github.com>
2020-08-25 08:41:09 +0900

    Update BattleSystem.cs

--- Turn_Based_Game/BattleSystem.cs
+++ Turn_Based_Game/BattleSystem.cs
@@ -1,12 +1,12 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }  //배틀을 나타내는 상태 5가지
+//배틀을 나타내는 상태
+public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST,ESCAPE } 
 
-public class BattleSystem : MonoBehaviour
-{
+public class BattleSystem : MonoBehaviour {
 	// 주인공과 적 가져오기
 	public GameObject playerPrefab;
 	public GameObject enemyPrefab;
@@ -19,19 +19,17 @@ public class BattleSystem : MonoBehaviour
 	// 대화창
 	public Text dialogueText;
 	//UI 표시용
-	public BattleHUD playerHUD;
-	public BattleHUD enemyHUD;
+	public BattleHud playerHUD;
+	public BattleHud enemyHUD;
 
 	public BattleState state;
 
-    void Start()
-    {
+	void Start() {
 		state = BattleState.START;    //첫 시작은 스타트
 		StartCoroutine(SetupBattle());
-    }
+	}
 
-	IEnumerator SetupBattle()
-	{
+	IEnumerator SetupBattle() {
 		GameObject playerGO = Instantiate(playerPrefab, playerBattleStation);
 		playerUnit = playerGO.GetComponent<Unit>();
 
@@ -49,45 +47,8 @@ public class BattleSystem : MonoBehaviour
 		PlayerTurn();
 	}
 
-	
 
-	IEnumerator EnemyTurn()
-	{
-		dialogueText.text = enemyUnit.unitName + "의 공격 !";
-
-		yield return new WaitForSeconds(1f);
-
-		bool isDead = playerUnit.TakeDamage(enemyUnit.damage);
-
-		playerHUD.SetHP(playerUnit.currentHP);
-
-		yield return new WaitForSeconds(1f);
-
-		if(isDead)
-		{
-			state = BattleState.LOST;
-			EndBattle();
-		} else
-		{
-			state = BattleState.PLAYERTURN;
-			PlayerTurn();
-		}
-
-	}
-
-	void EndBattle()
-	{
-		if(state == BattleState.WON)
-		{
-			dialogueText.text = "배틀에서 이겼다!";
-		} else if (state == BattleState.LOST)
-		{
-			dialogueText.text = "배틀에서 패배했다.";
-		}
-	}
-
-	void PlayerTurn()
-	{
+	void PlayerTurn() {
 		dialogueText.text = "무얼 해야할까 : ";
 	}
 	IEnumerator PlayerAttack() {
@@ -108,8 +69,7 @@ public class BattleSystem : MonoBehaviour
 		}
 	}
 
-	IEnumerator PlayerHeal()
-	{
+	IEnumerator PlayerHeal() {
 		playerUnit.Heal(5);
 
 		playerHUD.SetHP(playerUnit.currentHP);
@@ -120,6 +80,14 @@ public class BattleSystem : MonoBehaviour
 		state = BattleState.ENEMYTURN;
 		StartCoroutine(EnemyTurn());
 	}
+	IEnumerator PlayerEscape() {
+		dialogueText.text = "도망치기!";
+
+		yield return new WaitForSeconds(2f);
+
+		state = BattleState.ESCAPE;
+		EndBattle();
+	}
 
 	public void OnAttackButton() {
 		// 플레이어턴이 아니라면 반환 X
@@ -129,12 +97,56 @@ public class BattleSystem : MonoBehaviour
 		StartCoroutine(PlayerAttack());
 	}
 
-	public void OnHealButton()
-	{
+	public void OnHealButton() {
 		if (state != BattleState.PLAYERTURN)
 			return;
 
 		StartCoroutine(PlayerHeal());
 	}
+
+	public void OnEscapeButton() {
+		if (state != BattleState.PLAYERTURN)
+			return;
+
+		StartCoroutine(PlayerEscape());
+	}
+
+
+
+	// 적과 관련된 함수
+	IEnumerator EnemyTurn() {
+		dialogueText.text = enemyUnit.unitName + "의 공격 !";
+
+		yield return new WaitForSeconds(1f);
+
+		bool isDead = playerUnit.TakeDamage(enemyUnit.damage);
+
+		playerHUD.SetHP(playerUnit.currentHP);
+
+		yield return new WaitForSeconds(1f);
+
+		if (isDead) {
+			state = BattleState.LOST;
+			EndBattle();
+		} else {
+			state = BattleState.PLAYERTURN;
+			PlayerTurn();
+		}
+
+	}
+
+
+	//배틀 끝
+	void EndBattle() {
+		if (state == BattleState.WON) {
+			dialogueText.text = "배틀에서 이겼다!";
+		} else if (state == BattleState.LOST) {
+			dialogueText.text = "배틀에서 패배했다.";
+		} else if (state == BattleState.ESCAPE) {
+			dialogueText.text = "성공적으로 도망쳤다.";
+		}
+	}
+
+	
 
 }
