f81a5bd90bfbd8db6a3cae4592dfb44703f3f6de
JangHanjun <68003176+JangHanjun@users.noreply.github.com>
2020-08-25 08:39:39 +0900

    Update Teleport.cs

--- Teleport.cs
+++ Teleport.cs
@@ -22,7 +22,7 @@ public class Teleport : MonoBehaviour
 
     private void OnTriggerStay2D(Collider2D collision) {
         if (collision.CompareTag("Player") && Input.GetKeyDown(KeyCode.LeftControl) ){
-            Invoke("StartTeleport", 0.5f);
+            Invoke("StartTeleport", 0.3f);
             Fade();
         }
     }
@@ -52,7 +52,7 @@ public class Teleport : MonoBehaviour
         }
         time = 0f;
 
-       yield return new WaitForSeconds(0.2f);
+       yield return new WaitForSeconds(0.3f);
         //밝아지기
         while (alpha.a > 0f) {
             time += Time.deltaTime / F_time;
@@ -64,15 +64,3 @@ public class Teleport : MonoBehaviour
         yield return null;
     }
 }
-
-
-/*
-사용방법
- 하이어라이키 - ui - image 생성 후 이미지를 검은색, 투명하게 조정
- 이후 인스펙터 창에 Panel에 그 이미지를 넣으면 페이드인아웃 효과 완성
- 
- 텔레포드의 경우 왼쪽 컨트롤키에 반응하게 만들었으며
- 텔레포트용 스프라이트에 콜라이더 추가 후 is trigger 한 뒤 원하는 위치에 있는 오브젝트를 To obj에 넣으면 됨.
- 간단한 방식이고 한 씬 내에서 이동하는 방법이니 마을 안 집 내부로 이동할때 같은 간단한 작업에 
-
-*/
