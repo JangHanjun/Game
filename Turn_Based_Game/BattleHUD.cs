ecd2d3e2212ef799de095b040637415056808e6b
JangHanjun <68003176+JangHanjun@users.noreply.github.com>
2020-09-29 14:46:10 +0900

    Update BattleHUD.cs

--- Turn_Based_Game/BattleHUD.cs
+++ Turn_Based_Game/BattleHUD.cs
@@ -1,26 +1,30 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class BattleHUD : MonoBehaviour
-{
+public class BattleHud : MonoBehaviour {
 	// UI를 위해 가져오는 것
 	public Text nameText;
-	public Text levelText;
 	public Slider hpSlider;
+	public Slider mpSlider;
 
-	public void SetHUD(Unit unit)
-	{
+	public void SetHUD(Unit unit) {
 		nameText.text = unit.unitName;
-		levelText.text = "Lv1 " + unit.unitLevel;
+
 		hpSlider.maxValue = unit.maxHP;
 		hpSlider.value = unit.currentHP;
+
+		mpSlider.maxValue = unit.maxMP;
+		mpSlider.value = unit.currentMP;
 	}
 
-	public void SetHP(int hp)
-	{
+	public void SetHP(int hp) {
 		hpSlider.value = hp;
+	}
+
+	public void SetMP(int mp) {
+		mpSlider.value = mp;
 	}
 
 }
