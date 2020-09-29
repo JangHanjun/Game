22ac59225449349ce52a8ebe02cdf8b896cc0675
JangHanjun <68003176+JangHanjun@users.noreply.github.com>
2020-09-29 14:46:42 +0900

    Update Unit.cs

--- Turn_Based_Game/Unit.cs
+++ Turn_Based_Game/Unit.cs
@@ -1,21 +1,29 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Unit : MonoBehaviour
-{
+public class Unit : MonoBehaviour {
 
 	public string unitName;
-	public int unitLevel;
 
-	public int damage;
-
+	//HP,MP관련
 	public int maxHP;
 	public int currentHP;
+	public int maxMP;
+	public int currentMP;
 
-	public bool TakeDamage(int dmg)
-	{
-		currentHP -= dmg;
+	//공격, 방어 관련
+	public int damage;
+	public int Mdamage;
+	public int defense;
+
+	//레벨 관련
+	public int currentLevel;
+	public int nextLevel;
+
+
+	public bool TakeDamage(int dmg, int def) {
+		currentHP = currentHP - dmg + def;
 
 		if (currentHP <= 0)
 			return true;
@@ -23,8 +31,19 @@ public class Unit : MonoBehaviour
 			return false;
 	}
 
-	public void Heal(int amount)
-	{
+	public bool TakeMDamage(int dmg, int def) {
+		currentHP = currentHP - dmg + def;
+
+		if (currentHP <= 0)
+			return true;
+		else
+			return false;
+	}
+	public void AfterMAttack() {
+		currentMP = currentMP - 5;
+	}
+
+	public void Heal(int amount) {
 		currentHP += amount;
 		if (currentHP > maxHP)
 			currentHP = maxHP;
