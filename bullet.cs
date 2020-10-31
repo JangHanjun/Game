2686573bc5552afcb14dbf5514688c80078d8467
husk <68003176+JangHanjun@users.noreply.github.com>
2020-10-31 20:35:02 +0900

    Update bullet.cs

--- bullet.cs
+++ bullet.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-public class bullet : MonoBehaviour
-{
+public class bullet : MonoBehaviour {
     public float speed;                                     // 공속
     public float destoryTime;                   // 사정거리
 
     public float distance;
     public LayerMask layer;
 
-    void Start(){
+    MonsterMove monsterMove;
+    private void Awake() {
+        //monsterMove = GetComponent<MonsterMove>();
+    }
+    void Start() {
         Destroy(gameObject, destoryTime);
     }
     void Update() {
@@ -19,10 +22,12 @@ public class bullet : MonoBehaviour
 
         // Destory bullet 
         RaycastHit2D ray = Physics2D.Raycast(transform.position, transform.right, distance, layer);
-        if(ray.collider != null){
+        if (ray.collider != null) {
             Destroy(gameObject);
-            if(ray.collider.tag == "Enemy") {
+            if (ray.collider.tag == "Enemy") {
+                monsterMove = ray.collider.GetComponent<MonsterMove>();
                 Debug.Log("Enemy Hit");
+                monsterMove.Damaged();
             }
         }
     }
