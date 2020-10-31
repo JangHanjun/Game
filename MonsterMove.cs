f52ef4f2da7b561405e94af46a8a4e53689f8b84
husk <68003176+JangHanjun@users.noreply.github.com>
2020-10-31 18:14:33 +0900

    Update MonsterMove.cs

--- MonsterMove.cs
+++ MonsterMove.cs
@@ -6,15 +6,21 @@ public class MonsterMove : MonoBehaviour
 {
     Rigidbody2D rigid;
     SpriteRenderer spriteRenderer;
+    BoxCollider2D collider;
+    PlayerMove playerMove;
+    Vector3 dirVec;
     public int moveDir;    // Moving direction, Random
     public int moveF;
+    public float monsterHp;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        collider = GetComponent<BoxCollider2D>();
+        playerMove = GetComponent<PlayerMove>();
         monsterAI();
-        // todo : flip
+        monsterHp = 3;
     }
     void Update() {
         if (rigid.velocity.x > 0.1f) {
@@ -23,17 +29,21 @@ public class MonsterMove : MonoBehaviour
             spriteRenderer.flipX = false;
         }
 
+        if (moveDir == -1)
+            dirVec = Vector3.left;
+        else if (moveDir == 1)
+            dirVec = Vector3.right;
+
     }
     void FixedUpdate()
     {
         rigid.velocity = new Vector2(moveDir, rigid.velocity.y);   // no jump monster
 
-        // raycast for detect fall
-        Vector2 monsterVec = new Vector2(rigid.position.x + moveDir, rigid.position.y);
-        Debug.DrawRay(monsterVec, Vector3.down, new Color(1, 0, 0));   // for debug
-        RaycastHit2D raycastHit = Physics2D.Raycast(monsterVec, Vector3.down, 1, LayerMask.GetMask("Ground"));
-        if(raycastHit.collider == null) {
-            if (raycastHit.distance < 0.5f) {
+        //Player Scan Ray
+        Debug.DrawRay(rigid.position, dirVec, new Color(1, 0, 0));
+        RaycastHit2D monsterRay = Physics2D.Raycast(rigid.position, dirVec, 4f, LayerMask.GetMask("Wall"));
+        if(monsterRay.collider != null) {
+            if (monsterRay.distance < 0.5f) {
                 moveDir = -moveDir;  // Change direction
             }
         }
@@ -42,6 +52,26 @@ public class MonsterMove : MonoBehaviour
     void monsterAI() {
         moveDir = Random.Range(-1, 2);   // -1<= ranNum <2
         // change frequency,  2 can be random num like moveDir   or    public float to see in inspector
+
         Invoke("monsterAI", moveF);
+    }
+    // Monster Damage
+    void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.tag == "bullet") {
+            Damaged();
+        }
+    }
+    // Damaged
+    public void Damaged() {
+        monsterHp = monsterHp - 1; // playerMove.power;
+        if (monsterHp <= 0) {
+            spriteRenderer.color = new Color(1, 1, 1, 0.4f);
+            collider.enabled = false;
+            Invoke("Destory", 3f);
+        }
+    }
+
+    void Destory() {
+        gameObject.SetActive(false);
     }
 }
