7c75dc18b79e96d764332f57220ef34952fce1d0
husk <68003176+JangHanjun@users.noreply.github.com>
2020-10-31 16:51:09 +0900

    Update MonsterMove.cs

--- MonsterMove.cs
+++ MonsterMove.cs
@@ -5,23 +5,32 @@ using UnityEngine;
 public class MonsterMove : MonoBehaviour
 {
     Rigidbody2D rigid;
+    SpriteRenderer spriteRenderer;
     public int moveDir;    // Moving direction, Random
     public int moveF;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
         monsterAI();
         // todo : flip
     }
+    void Update() {
+        if (rigid.velocity.x > 0.1f) {
+            spriteRenderer.flipX = true;
+        } else {
+            spriteRenderer.flipX = false;
+        }
 
+    }
     void FixedUpdate()
     {
         rigid.velocity = new Vector2(moveDir, rigid.velocity.y);   // no jump monster
 
         // raycast for detect fall
         Vector2 monsterVec = new Vector2(rigid.position.x + moveDir, rigid.position.y);
-        //Debug.DrawRay(monsterVec, Vector3.down, new Color(1, 0, 0));   // for debug
+        Debug.DrawRay(monsterVec, Vector3.down, new Color(1, 0, 0));   // for debug
         RaycastHit2D raycastHit = Physics2D.Raycast(monsterVec, Vector3.down, 1, LayerMask.GetMask("Ground"));
         if(raycastHit.collider == null) {
             if (raycastHit.distance < 0.5f) {
