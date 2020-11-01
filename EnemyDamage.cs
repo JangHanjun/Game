4d05893381ad23fa6e66be288ebe976e2e2eaf45
husk <68003176+JangHanjun@users.noreply.github.com>
2020-11-01 16:02:49 +0900

    Create EnemyDamage.cs

---
+++ EnemyDamage.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    public float maxHp;
+    public float curHp;
+
+    Rigidbody2D rigid;
+    BoxCollider2D boxCollider2D;
+    SpriteRenderer spriteRenderer;
+    void Awake() {
+        rigid = GetComponent<Rigidbody2D>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void Damaged() {
+        if (curHp <= 0) {
+            spriteRenderer.color = new Color(1, 1, 1, 0.4f);
+        }
+    }
+}
