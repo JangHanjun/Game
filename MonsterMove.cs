ef06c593df13c76fd92b84d9c21ae19b1b0858b1
husk <68003176+JangHanjun@users.noreply.github.com>
2020-11-01 16:02:01 +0900

    Update MonsterMove.cs

--- MonsterMove.cs
+++ MonsterMove.cs
@@ -2,8 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MonsterMove : MonoBehaviour
-{
+public class MonsterMove : MonoBehaviour {
     Rigidbody2D rigid;
     SpriteRenderer spriteRenderer;
     BoxCollider2D collider;
@@ -11,16 +10,11 @@ public class MonsterMove : MonoBehaviour
     Vector3 dirVec;
     public int moveDir;    // Moving direction, Random
     public int moveF;
-    public float monsterHp;
 
-    void Awake()
-    {
+    void Awake() {
         rigid = GetComponent<Rigidbody2D>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
         collider = GetComponent<BoxCollider2D>();
-        playerMove = GetComponent<PlayerMove>();
         monsterAI();
-        monsterHp = 3;
     }
     void Update() {
         if (rigid.velocity.x > 0.1f) {
@@ -35,14 +29,13 @@ public class MonsterMove : MonoBehaviour
             dirVec = Vector3.right;
 
     }
-    void FixedUpdate()
-    {
+    void FixedUpdate() {
         rigid.velocity = new Vector2(moveDir, rigid.velocity.y);   // no jump monster
 
         //Player Scan Ray
         Debug.DrawRay(rigid.position, dirVec, new Color(1, 0, 0));
         RaycastHit2D monsterRay = Physics2D.Raycast(rigid.position, dirVec, 4f, LayerMask.GetMask("Wall"));
-        if(monsterRay.collider != null) {
+        if (monsterRay.collider != null) {
             if (monsterRay.distance < 0.5f) {
                 moveDir = -moveDir;  // Change direction
             }
@@ -54,24 +47,5 @@ public class MonsterMove : MonoBehaviour
         // change frequency,  2 can be random num like moveDir   or    public float to see in inspector
 
         Invoke("monsterAI", moveF);
-    }
-    // Monster Damage
-   /* void OnCollisionEnter2D(Collision2D collision) {
-        if (collision.gameObject.tag == "bullet") {
-            Damaged();
-        }
-    } */
-    // Damaged
-    public void Damaged() {
-        monsterHp = monsterHp - 1; // playerMove.power;
-        if (monsterHp <= 0) {
-            spriteRenderer.color = new Color(1, 1, 1, 0.4f);
-            collider.enabled = false;
-            Invoke("Destory", 3f);
-        }
-    }
-
-    void Destory() {
-        gameObject.SetActive(false);
     }
 }
