daa7b2f2f57286f05f2eb277c8775a202c99e2a4
husk <68003176+JangHanjun@users.noreply.github.com>
2020-11-01 15:14:06 +0900

    Update bullet.cs

--- bullet.cs
+++ bullet.cs
@@ -1,3 +1,32 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class bullet : MonoBehaviour {
+    public float speed;                                     // 공속
+    public float destoryTime;                   // 사정거리
+    public float power;                                    // 공격력
+
+    public LayerMask layer;
+
+    //MonsterMove monsterMove;
+    void Start() {
+        Destroy(gameObject, destoryTime);
+    }
+    void Update() {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+        // Destory bullet 
+        RaycastHit2D ray = Physics2D.Raycast(transform.position, transform.right, 0f, layer);
+        if (ray.collider != null) {
+            Destroy(gameObject);
+        }
+    }
+}
+
+/*  총알이 떄리는 것에 초점을 둔 스크립트
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
@@ -29,3 +58,4 @@ public class bullet : MonoBehaviour {
         }
     }
 }
+*/
